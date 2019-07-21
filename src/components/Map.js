f8e79e1ac83e1335aab61def182b88d490d51fe4
Stuart Morris <admin@handcode.co.uk>
2019-07-21 22:52:06 +0100

    Map showing user name in correct locations from dummy data

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,10 +3,21 @@ import GoogleMapReact from "google-map-react";
 
 import StyledMap from "./styles/Map";
 
-const Map = () => {
+const MapMarker = ({ user }) => <div>{user.name}</div>;
+
+const Map = ({ data }) => {
     return (
         <StyledMap>
-            <GoogleMapReact defaultCenter={{ lat: -37.3159, lng: 81.1496 }} defaultZoom={3} />
+            <GoogleMapReact defaultCenter={{ lat: -37.3159, lng: 81.1496 }} defaultZoom={3}>
+                {data.map(user => (
+                    <MapMarker
+                        key={user.id}
+                        lat={user.address.geo.lat}
+                        lng={user.address.geo.lng}
+                        user={user}
+                    />
+                ))}
+            </GoogleMapReact>
         </StyledMap>
     );
 };
