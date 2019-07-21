1a5da193e912e2c1f41683bd60f4eac7119793a3
Stuart Morris <admin@handcode.co.uk>
2019-07-21 22:44:10 +0100

    Basic map displayed using google-map-react, small styling added to Map using styled-components

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,13 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+
+import StyledMap from "./styles/Map";
 
 const Map = () => {
     return (
-        <div>
-            <h1>Map</h1>
-        </div>
+        <StyledMap>
+            <GoogleMapReact defaultCenter={{ lat: -37.3159, lng: 81.1496 }} defaultZoom={3} />
+        </StyledMap>
     );
 };
 
