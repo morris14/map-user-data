27c661d039b0cfb3304f81eae5d27ba08795a7a2
Stuart Morris <admin@handcode.co.uk>
2019-07-21 23:04:37 +0100

    Fetching data with axios for the map to render users locations

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
+import axios from "axios";
 
 import StyledMap from "./styles/Map";
 
 const MapMarker = ({ user }) => <div>{user.name}</div>;
 
-const Map = ({ data }) => {
+const Map = () => {
+    const [data, setdata] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then(res => setdata(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
     return (
         <StyledMap>
-            <GoogleMapReact defaultCenter={{ lat: -37.3159, lng: 81.1496 }} defaultZoom={3}>
-                {data.map(user => (
-                    <MapMarker
-                        key={user.id}
-                        lat={user.address.geo.lat}
-                        lng={user.address.geo.lng}
-                        user={user}
-                    />
-                ))}
-            </GoogleMapReact>
+            {data ? (
+                <GoogleMapReact defaultCenter={{ lat: -37.3159, lng: 81.1496 }} defaultZoom={3}>
+                    {data.map(user => (
+                        <MapMarker
+                            key={user.id}
+                            lat={user.address.geo.lat}
+                            lng={user.address.geo.lng}
+                            user={user}
+                        />
+                    ))}
+                </GoogleMapReact>
+            ) : (
+                "Fetching map data..."
+            )}
         </StyledMap>
     );
 };
