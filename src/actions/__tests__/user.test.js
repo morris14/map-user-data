04e6b3c549d2e83b0691356e085b881637188dcf
Stuart Morris <admin@handcode.co.uk>
2019-07-22 00:58:07 +0100

    Added actions tests and modified payload in user reducer tests

---
+++ src/actions/__tests__/user.test.js
@@ -0,0 +1,22 @@
+import { setCurrentUser } from "../user";
+import { SET_CURRENT_USER } from "../types";
+
+describe("setCurrentUser", () => {
+    it("has the correct type", () => {
+        const action = setCurrentUser();
+
+        expect(action.type).toEqual(SET_CURRENT_USER);
+    });
+
+    it("has the correct payload", () => {
+        const payload = {
+            type: SET_CURRENT_USER,
+            payload: {
+                name: "Leanne Graham",
+            },
+        };
+        const action = setCurrentUser(payload);
+
+        expect(action.payload).toEqual(payload);
+    });
+});
